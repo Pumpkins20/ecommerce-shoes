f644c36b654ac4b4c367b3daf4569f608436f4a4
Pumpkins20 <firdausalamanda90@gmail.com>
2024-07-13 10:20:16 +0700

    ecommerce-shoes

---
+++ lib/Widget/Components/rounded_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final VoidCallback press;
+  final Color color, textColor;
+
+  const RoundedButton({
+    Key? key,
+    required this.text,
+    required this.press,
+    required this.color,
+    this.textColor = Colors.white,
+  }) : super(key: key);
+
+  get kPrimaryColor => null;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      width: size.width,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: TextButton(
+          style: TextButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+              foregroundColor: Color.fromRGBO(244, 234, 234, 1)),
+          onPressed: press,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
